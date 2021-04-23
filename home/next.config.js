7de2a02e5018407336fd5ff4ebf0850978785d28
William Gordon <gordon@planetscale.com>
2021-04-23 09:50:29 -0400

    test website rewrite

--- home/next.config.js
+++ home/next.config.js
@@ -1,16 +1,20 @@
-const { BLOG_URL } = process.env
+const { BLOG_URL } = process.env;
 
 module.exports = {
   rewrites() {
     return [
       {
-        source: '/blog',
+        source: "/blog",
         destination: `${BLOG_URL}/blog`,
       },
       {
-        source: '/blog/:path*',
+        source: "/blog/:path*",
         destination: `${BLOG_URL}/blog/:path*`,
       },
-    ]
+      {
+        source: "/about",
+        destination: "https://planetscale.com/about",
+      },
+    ];
   },
-}
+};
